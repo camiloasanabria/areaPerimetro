dc0e38b4eec07774f0e6a34c2547444abf1a1100
Unknown <cass465andres@gmail.com>
2018-02-15 13:21:54 -0500

    triangulo

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main{
 	public static void main(String[] arg){
-		//pi r al cuadrado
-		//b*h/2
+		//r es el radio del circulo
+		// a,b,c son los lados del triangulo donde b es la base y h es la altura
 		int r=10;
-		System.out.println("radio del circulo r="+r);
+		int a=3;
+		int b=4;
+		int c=5;
+		int h=6;
 		System.out.println("el area del circulo con r="+r+" es: "+(3.1416*(r*r)));
-		System.out.println("el perimetro del circulo con r="+r+" es: "+(2*3.1416*r));
+		System.out.println("el perimetro del circulo con r="+r+" es: "+(2*3.1416*r)+"\n\n");
+		System.out.println("el area del triangulo con b="+b+" y h="+h+" es: "+((b*h)/2));
+		System.out.println("el perimetro del triangulo con lados a="+a+" b="+b+" c="+c+" es: "+(a+b+c)+"\n\n");
 	}
 }
