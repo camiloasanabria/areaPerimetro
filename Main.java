00f331226897d37b384ba4a021932f825c17da8a
Unknown <cass465andres@gmail.com>
2018-02-15 13:02:07 -0500

    Circulo

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String[] arg){
+		//pi r al cuadrado
+		//b*h/2
+		int r=10;
+		System.out.println("radio del circulo r="+r);
+		System.out.println("el area del circulo con r="+r+" es: "+(3.1416*(r*r)));
+		System.out.println("el perimetro del circulo con r="+r+" es: "+(2*3.1416*r));
+	}
+}
